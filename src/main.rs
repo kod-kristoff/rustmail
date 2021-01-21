7244ed49897c300df61055db08056b34478eb223
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-01-21 13:57:56 +0100

    feat: add lettre 0.10.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use lettre::{
+    Message,
+    stub::StubTransport
+};
+
 fn main() {
     println!("Hello, world!");
+
+    let email = Message::builder()
+        .from("NoBody <nobody@domain.tld>".parse().unwrap())
+        .reply_to("Yuin <yuin@domain.tld>".parse().unwrap())
+        .to("Hei <hei@domain.tld>".parse().unwrap())
+        .subject("Happy new async year")
+        .body(String::from("Be happy with async!"))
+        .unwrap();
 }
